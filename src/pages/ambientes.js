f65ad8cd632b64986f568a7d0f9232fc326c406e
Beatriz Almeida Lins Lopes <beatriz.lopes@PEINSMTNB2661.insole.local>
2020-06-18 16:34:23 -0300

    Trabalhando no componente Flatlist em Ambientes

--- src/pages/ambientes.js
+++ src/pages/ambientes.js
@@ -1,25 +1,81 @@
 import React  from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  ScrollView,
-  TextInput,
-} from 'react-native';
+import { StyleSheet, View, Text, ScrollView, TextInput, Image, Button,  FlatList, SafeAreaView,} from 'react-native';
 
 const Ambientes = () => {
   return (
-   <ScrollView style={styles.body}>
-        <Text style={styles.titulo}>
+   <View style={styles.body}>
+     <View style={styles.header}>
+     {/* <Button style ={styles.Addbutton}
+              onPress={() => alert('This is a button!')}
+              title=""
+              color="red"
+            /> */}
+     </View>
+      <Text style={styles.titulo}>
            Ambientes
-        </Text>
-        <TextInput style={styles.campoPesquisa}>
-         Placeholder Text
-        </TextInput>    
-   </ScrollView>
+      </Text>
+      <TextInput style={styles.campoPesquisa}>
+          <Image style={styles.icone} source={require('./../images/icons/buscar.png')}/>
+           Buscar Ambiente ... 
+      </TextInput> 
+          <ListaAmbientes></ListaAmbientes>
+   </View>
   );
 };
 
+// Criando o componente Lista
+
+class ListaAmbientes extends React.Component {
+  state = {
+    data: [
+      { id: "0", name: "Escritório", qtdMod:2 },
+      { id: "1", name: "Estacionamento", qtdMod:4 },
+      { id: "2", name: "Expedição", qtdMod:1 },
+    ]
+  };
+  qtdAmb = this.state.data.length - 1;
+  render() {
+    return (
+      <SafeAreaView>
+        <View style={styles.grupo}>
+        <FlatList
+          data={this.state.data}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => {
+            if(item.id <this.qtdAmb){
+              return (
+                <View style={styles.item}>
+                   {/* <Image style={styles.iconeFavorito} source={require('./../images/icons/naoFavorito.png')}/> */}
+                 <Text style={styles.text}>
+                   {item.name} 
+                   </Text>
+                   <Text style={styles.infoQuantidade}>{item.qtdMod}
+                   <Image source={require('./../images/icons/setaDireita.png')}/>
+                   </Text>
+                 
+                </View>
+                     );}
+            else{  
+              return (
+                <View style={styles.itemFinal}>
+                  <Text style={styles.text}>{item.name}</Text>
+                  <Text style={styles.infoQuantidade}>{item.qtdMod}
+                   <Image source={require('./../images/icons/setaDireita.png')}/>
+                   </Text>
+                </View>
+              );
+
+            }
+           
+          }}
+        />
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+// Estilização dos componentes 
 const styles = StyleSheet.create({
  titulo:{
    color:"white",
@@ -32,22 +88,71 @@ const styles = StyleSheet.create({
    left: 16,
    right: 16,
    top:10,
+   marginBottom:18,
  },
  body: {
    flex:1,
    backgroundColor: "#000000",
  },
  campoPesquisa:{
-  padding: 10,
-  width: 343,
+  height:44,
+  padding: 12,
+  // Deveria ser padding: 10px 12px
+  width:"90%",
   left: 16,
-  top: 18,
-  backgroundColor: "rgba(255, 255, 255, 0.13)",
+  backgroundColor: "rgba(255, 255, 255,0.13)",
   borderRadius: 10,
   fontSize: 17,
   letterSpacing: -0.408,
   color: "rgba(255, 255, 255, 0.3)",
  },
+ icone:{
+  width: 24,
+  height: 24,
+ },
+ header:{
+  height: 54,// 54 + 24 = 78
+ },
+ Addbutton:{
+   backgroundColor:"red",
+ },
+ // Estilização da Lista:
+ grupo:{
+  backgroundColor: "linear-gradient(0deg, rgba(255, 255, 255, 0.11), rgba(255, 255, 255, 0.11))",
+  borderRadius: 12,
+  width:"90%",
+  alignSelf:"center",
+  marginTop:52,
+ },
+ item: {
+  height: 48,
+  borderBottomColor: "rgba(255, 255, 255, 0.12)",
+  borderBottomWidth: 1,
+},
+itemFinal: {
+  height: 48,
+},
+text: {
+  color: "#FFFFFF",
+  fontSize: 17,
+  position: "absolute",
+  // width: 183,
+  height: 24,
+  left: 8,
+  top: 12,
+  marginLeft:8,
+  lineHeight: 22,
+  letterSpacing: -0.408, 
+},
+infoQuantidade: {
+  textAlign: "right",
+  color: "rgba(235, 235, 245, 0.6)",
+  marginHorizontal:4,
+},
+iconeFavorito:{
+//  alignSelf:"flex-start",
+}
 });
 
+// Exportando o componente (página) Ambientes:
 export default Ambientes;
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,18 +1,66 @@
 import React  from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useIsFocused } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ambientes from "./ambientes";
-import Automacoes from "./automacoes";
-import {
-  StyleSheet,
-} from 'react-native';
+import Ambientes from "./ambientes"; // Importando a página Ambientes
+import Automacoes from "./automacoes"; // Importando a página Automações
+import { StyleSheet, Image } from 'react-native'; // Importando os componentes que vão ser usados
 
-const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator(); // Criando o menu inferior
 
 const App = ({ navigation }) => {
   return (
   <NavigationContainer>
-   <Tab.Navigator style={styles.menuInferior}>
+   <Tab.Navigator 
+   screenOptions={({route}) => ({
+    tabBarIcon:({focused}) => {
+
+      if(route.name == 'Ambientes'){ // Se for o tab da rota Ambientes:
+        if(focused){
+          return <Image source={require('./../images/icons/ambientesAtivo.png')}/>; // Se estiver selecionado
+        } else {
+          return <Image source={require('./../images/icons/ambientes.png')}/> // Se não estiver selecionado
+        };
+        
+      }
+      if(route.name == 'Automações'){
+        if(focused){
+          return <Image source={require('./../images/icons/automacoesAtivo.png')}/>;
+        } else {
+          return <Image source={require('./../images/icons/automacoes.png')}/>
+        };
+      }
+      if(route.name == 'Consumo'){
+        if(focused){
+          return <Image source={require('./../images/icons/consumo.png')}/>;
+        } else {
+          return <Image source={require('./../images/icons/consumo.png')}/>
+        };
+      }
+      if(route.name == 'Configurações'){
+        if(focused){
+          return <Image source={require('./../images/icons/configuracoes.png')}/>;
+        } else {
+          return <Image source={require('./../images/icons/configuracoes.png')}/>
+        };
+      }
+    }
+
+   })}
+   tabBarOptions={{ // Algumas configurações de estilo do menu inferior
+    activeTintColor:'rgb(241,104,37)', 
+    inactiveTintColor:'rgb(101,101,101)',
+    style: {                    
+      backgroundColor: "#000000", 
+      borderTopColor: 'transparent',
+     },  
+     labelStyle: {        
+      fontSize: 11,   
+      fontWeight: 'normal',  
+      fontFamily: "Barlow",     
+     },
+
+   }}
+   >
         <Tab.Screen name="Ambientes" component={Ambientes} />
         <Tab.Screen name="Automações" component={Automacoes} />
         <Tab.Screen name="Consumo" component={Automacoes} />
@@ -21,12 +69,5 @@ const App = ({ navigation }) => {
  </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
- menuInferior:{
-  backgroundColor:"#000000",
-  fontSize: 11,
- }
-});
 
 export default App;
