09296b64e6d4d99f406b042d36d401391d71754c
Beatriz Almeida Lins Lopes <beatriz.lopes@PEINSMTNB2661.insole.local>
2020-07-06 17:49:57 -0300

    Trabalhando no modal de editar ambiente

--- src/pages/ambiente.js
+++ src/pages/ambiente.js
@@ -15,6 +15,8 @@ var listaAutomacoes = [
 var listaDispositivos = [
   {nome:"Ar-Condicionado Digital Inverter 17,000 Btu/h Frio 8-Polo", marca:"Sansung", modelo:"AR18NVFPCWKNAZ", cod:"1", temperatura: 24, velocidade:3},
   {nome:"Ar-Condicionado Split Hi Wall LG Dual Inverter Voice 12000 ", marca:"Sansung", modelo:"S4-W12JA31A", cod:"2", temperatura:17,velocidade:2},
+  {nome:"Ar-Condicionado Split Hi Wall LG  Inverter ", marca:"Sansung", modelo:"S4-W12JA31B", cod:"3", temperatura:18,velocidade:2},
+
 ]
 var ambiente = listaAmbientes[0];
 
@@ -52,36 +54,35 @@ class ListaAuto extends React.Component {
       return(
         <View style={styles.dispositivo} key={dispositivo.cod}>
           <Image  source={buscaImagem} style={{left: "5%",top:"9%"}}/>
-        <TouchableWithoutFeedback  onPress={() => {setModalVisible(true);}}>
-        <Text style={styles.dispositivoNome}>{dispositivo.nome} </Text>
-        </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback  onPress={() => {setModalVisible(true);}}>
+            <Text style={styles.dispositivoNome}>{dispositivo.nome} </Text>
+          </TouchableWithoutFeedback>
         
-        <Text style={styles.dispositivoModelo}>{dispositivo.modelo}</Text>  
-    
+          <Text style={styles.dispositivoModelo}>{dispositivo.modelo}</Text>  
+    {/* Início de Controle */}
         <Modal animationType="slide" transparent={true} visible={modalVisible} >
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
 
             <Text style={styles.controleTitulo}>Controle Remoto</Text>
-
+            {/* Botão de fechar o controle */}
             <TouchableWithoutFeedback onPress={() => { setModalVisible(!modalVisible);}}>
               <Image style={styles.closeButton} source={require('../images/controle/fecharControle.png')}/> 
             </TouchableWithoutFeedback> 
-
+            {/* Info sobre o controle */}
             <Text style={styles.controleNome}>{dispositivo.nome}</Text>
-
             <Text style={styles.controleModelo}>{dispositivo.modelo}</Text>
-
+            {/* Botão de Ligar/Desligar o dispositivo */}
             <Image source={require('../images/controle/ligaDesliga.png')} style={{position:"relative",top:"15%"}}/> 
-           
+           {/* Outros botões: */}
             <View style={{top:"20%",left:16, flex: 1,flexDirection:"row",flexWrap:"wrap",}}>
+             {/* Temperatura */}
              <View style={styles.controleTemperatura}>
               <Text style={{color:" rgba(255, 255, 255, 0.5)",fontSize:13}}>Temperatura</Text>
-              <View style={styles.visorTemperatura}>
+              <View style={styles.visor}>
                <Text style={styles.valorTemperatura}>{temperatura}ºC</Text>
               </View>   
-
-              <View style={styles.botoesTemperatura}>
+              <View style={styles.botoes}>
                 <TouchableWithoutFeedback onPress={() => { setTemperatura(temperatura + 1);} }>
                    <Image source={require('../images/controle/aumentaTemp.png')}/>
                 </TouchableWithoutFeedback>               
@@ -90,19 +91,20 @@ class ListaAuto extends React.Component {
                   <Image source={require('../images/controle/diminuiTemp.png')}/>
                 </TouchableWithoutFeedback>
               </View>   
-
             </View>
 
             <Image style={{top:0,right:"18%"}} source={require('../images/controle/divisorControle.png')}/> 
-            
+            {/* Velocidade do Vento */}
             <View>
               <Text style={{color:" rgba(255, 255, 255, 0.5)",fontSize:13}} >Velocidade do Vento</Text>
-              <View style={styles.visorVelocidade}>
-                <Image style={{top:"20%",marginEnd:"8%"}} source={require('../images/controle/ventoVelocidade.png')}/> 
-                <Image style={{top:"20%",marginEnd:"8%"}} source={require('../images/controle/ventoVelocidade.png')}/> 
-                <Image style={{top:"20%"}} source={require('../images/controle/ventoVelocidade.png')}/> 
-              </View> 
-              <View style={styles.botoesVelocidade}>
+              <View style={[styles.visor,{paddingVertical:10,flexDirection:"row", right:"0%"}]}>
+               <Image style={{marginEnd:"8%"}} source={require('../images/controle/ventoVelocidade.png')}/>
+               <Image style={{marginEnd:"8%"}} source={require('../images/controle/ventoVelocidade.png')}/>
+               <Image style={{marginEnd:"8%"}} source={require('../images/controle/ventoVelocidade.png')}/>
+             </View>
+                {/* <Visor velocidade={dispositivo.velocidade}></Visor>  */}
+              
+              <View style={[styles.botoes,{top:"18%",right:"2%",}]}>
               <Image source={require('../images/controle/aumentaVelocidade.png')}/>
                 <Image style={{marginHorizontal:5}} source={require('../images/controle/divisorBotoes.png')}/>
                 <Image source={require('../images/controle/diminuiVelocidade.png')}/>
@@ -112,16 +114,17 @@ class ListaAuto extends React.Component {
           </View>
         </View>
       </Modal>
-
+      {/* Fim do controle */}
         </View>
       )
     })
     return item;
         }
 
+
 // Componente referente à página Ambiente (que utiliza os componentes criados anteriormente) /////////////////////////////////////////////////
 const Ambiente = ({route,navigation}) => {  
- 
+  const [modalEditar, setModalEditar] = useState(false);
   const {ambienteSelecionado} = route.params;
   const {grupoSelecionado} = route.params;
   const {codAmbiente}=route.params;
@@ -133,7 +136,7 @@ const Ambiente = ({route,navigation}) => {
           <Image  style={{position: "absolute", top: "55%"}} source={require('./../images/icons/setaLaranjaEsq.png')}/>
         </TouchableWithoutFeedback>
         <Text style={styles.ambientes}> Ambientes  </Text>
-        <TouchableWithoutFeedback onPress={() => alert("Editar Ambiente")}>
+        <TouchableWithoutFeedback onPress={() => {setModalEditar(true);}}>
          <Text style={styles.editar}> Editar </Text>
         </TouchableWithoutFeedback> 
      </View> 
@@ -167,6 +170,22 @@ const Ambiente = ({route,navigation}) => {
           <Text style={{position:"relative", color: "white",fontSize: 15, left: "4.76%", right:"4.76%", top: "77%", letterSpacing: -0.24}}>Adicionar dispositivo</Text>
         </View> 
       </View>
+      {/* Modal de Editar */}
+      <Modal animationType="slide" transparent={true} visible={modalEditar} >
+        <View style={styles.centeredView}>
+          <View style={styles.modalEditar}>
+            <TouchableWithoutFeedback onPress={() => { setModalEditar(!modalEditar);}}>
+              <Text style={styles.botoesSuperior}> Cancelar  </Text>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={() => alert("Salvar Alterações")}>
+              <Text style={[styles.botoesSuperior,{left:"90%"}]}> Confirmar </Text>
+            </TouchableWithoutFeedback>
+            <Text style={styles.inputLabel}>NOME DO AMBIENTE</Text>
+            <TextInput style={styles.inputNome}></TextInput>
+        </View>
+    </View>
+  </Modal>
+      {/* Final modal */}
 
    </ScrollView>
   );
@@ -192,10 +211,6 @@ const styles = StyleSheet.create({
    flex:1,
    backgroundColor: "#000000",
  },
- icone:{
-  width: 24,
-  height: 24,
- },
  header:{
   height: 54,// 54 + 24 = 78
  },
@@ -207,13 +222,8 @@ const styles = StyleSheet.create({
   width: 44,
   fontWeight: "600",
   fontSize: 17,
-  lineHeight: 22,
- },
- Addbutton:{
-   backgroundColor:"red",
  },
  ambientes:{
-  position: "absolute",
   left: 24,
   top: "55%",
   color: "#F18929",
@@ -236,25 +246,23 @@ const styles = StyleSheet.create({
  automaçõesView:{
    left:16,
    marginTop:"8%",
-  
  },
  listaAutomações:{
    width:"100%",
    height:149, 
   },
-  automação:{
-    width: 248,
-    height: 126,
-    backgroundColor: "rgba(255, 255, 255,0.13)",
-    borderRadius: 12,
-    left: 4,
-    marginEnd:8,
-    marginTop:23,
-  },
+ automação:{
+  width: 248,
+  height: 126,
+  backgroundColor: "rgba(255, 255, 255,0.13)",
+  borderRadius: 12,
+  left: 4,
+  marginEnd:8,
+  marginTop:23,
+},
   automaçãoNome: {
     color: "white",
     fontSize: 17,
-    fontStyle:"normal",
     fontWeight: "600",
     left: 8,
     top: 8,
@@ -263,8 +271,6 @@ const styles = StyleSheet.create({
   automaçãoHorario:{
     color: ambiente.cor,
     fontSize: 11,
-    fontStyle:"normal",
-    fontWeight: "normal",
     left: 8, 
     top: 8
   },
@@ -284,7 +290,8 @@ const styles = StyleSheet.create({
     fontWeight: "normal", 
     left: 8, 
     top: 48,
-    lineHeight: 13},
+    lineHeight: 13
+  },
   dispositivosTitulo:{
     color: "white",
     fontSize: 17,
@@ -301,7 +308,6 @@ const styles = StyleSheet.create({
     flexWrap:"wrap",
   },
   dispositivo:{
-    //  width: 168,
     width:"45%",
     height: 126,
     backgroundColor: "rgba(255, 255, 255,0.13)",
@@ -401,7 +407,7 @@ const styles = StyleSheet.create({
     controleTemperatura: {
       width:"50%",
     },
-    visorTemperatura:{
+    visor:{
       height:45,
       width:127,
       paddingHorizontal:22,
@@ -417,7 +423,7 @@ const styles = StyleSheet.create({
       letterSpacing: 0.374,
       color: "#F18929",
     },
-    botoesTemperatura:{
+    botoes:{
       height:40,
       width:137,
       padding:4,
@@ -428,47 +434,42 @@ const styles = StyleSheet.create({
       flexDirection:"row",
       flexWrap:"wrap",
     },
-    visorVelocidade:{
-      height:45,
-      width:127,
-      paddingHorizontal:22,
-      paddingVertical:10,
-      backgroundColor:"rgba(241, 137, 41, 0.15)",
-      borderRadius:16,
-      top:"10%",   
-      flexDirection:"row",
-      flexWrap:"wrap",
-    },
-    botoesVelocidade:{
-      height:40,
-      width:137,
-      padding:4,
-      backgroundColor:"rgba(255, 255, 255, 0.1)",
-      borderRadius:100,
-      top:"18%",
-      right:"2%",
-      flexDirection:"row",
-      flexWrap:"wrap",
-    },
+// Modal de Editar Ambiente
+modalEditar: {
+  height:"100%",
+  width:"100%",
+  margin: 20,
+  backgroundColor:  "#1c1c1c",
+  padding: "9%",
+  alignItems: "center",
+  shadowColor: "#000",
 
-
+},
+botoesSuperior:{
+  position: "absolute",
+  left:"5%",
+  top: "8%",
+  color: "#F18929",
+  fontWeight: "600",
+  fontSize: 17,
+  lineHeight: 22,
+},
+inputLabel:{
+  fontSize: 13,
+  color:"rgba(255, 255, 255, 0.5)",
+  top:"10%",
+  right:"35%"
+},
+inputNome:{
+  position:"absolute",
+  width: "110%",
+  height: 42,
+  left: 16,
+  top: "21%", 
+  backgroundColor: "rgba(255, 255, 255, 0.13)",
+  borderRadius: 10,
+},
 })
-
-// var statusModal = function (status){
-//   if(status){
-//     return {
-      
-//       flex:1,
-//       backgroundColor: "#000000",
-//     }
-//   }
-//   else{
-//     return  {
-//       flex:1,
-//       backgroundColor: "#000000",
-//     }
-//   }
-// }
 
 // Exportando a página ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 export default Ambiente
