09ca88310083f5ee99dcbd427b63b69b45b02640
Beatriz Almeida Lins Lopes <beatriz.lopes@PEINSMTNB2661.insole.local>
2020-07-08 10:18:02 -0300

    Continuando a parte de listar automações

--- src/funcoes/listarAutomações.js
+++ src/funcoes/listarAutomações.js
@@ -4,6 +4,8 @@ var listaAutomacoes = [
     {tipo: "Gatilho",nome:"Hora Extra",dias:"09/07/2020", horario:"13:00",cod:"2"},
     {tipo: "Automação",nome:"Intervalo", dias:["SEG","TER","QUA","QUI","SEX"], horario:"14:00",cod:"3"},
     {tipo: "Gatilho",nome:"Teste",dias:"09/07/2020", horario:"15:00",cod:"4"},
+    {tipo: "Automação",nome:"Automação 2", dias:["SEG","QUA","QUI","SEX"], horario:"01:00",cod:"5"},
+
   ]
   var data = "07/07/2020";
   var hora = "13:30";
@@ -17,6 +19,19 @@ for (var i=0;i<listaAutomacoes.length;i++){
         if(ehHoje){
            listaAutomacoes[i].tempoRestante = AnalisaHorario(listaAutomacoes[i].horario);
            j = j +1;
+        } else {
+            faltamDias = AnalisaDia(listaAutomacoes[i].dias);
+            console.log("Dias")
+            console.log(faltamDias)
+            faltamMinutos = AnalisaHorario(listaAutomacoes[i].horario);
+            console.log("Minutos")
+            console.log(faltamMinutos)
+            if(faltamDias == -1){
+              // Nesse caso, se faltamDias e -1, significa que a próxima ocorrência é no dia seguinte. Ai ele soma a quantidade de minutos que faltam para acabar o dia com a qtd de minutos do inicio do evento no próx dia
+              faltam = (1440 - transformaHora(hora)) + transformaHora(listaAutomacoes[i].horario.split("/")[0])
+            }
+            faltam = parseInt(faltamDias) + parseInt(faltamMinutos);
+            listaAutomacoes[i].tempoRestante = faltam;
         }
     } 
     else {
@@ -36,6 +51,15 @@ function HojeTem (automacao){
     }
     return sim;
 }
+
+function transformaHora (horario1){
+    var horaMinuto = horario1.split(":");
+    var qtdHoras = parseInt(horaMinuto[0]);
+    var qtdMinutos = parseInt(horaMinuto[1]);
+    qtdMinutos = (qtdHoras*60) + qtdMinutos;
+    return qtdMinutos
+}
+
 function AnalisaHorario (horario){
     var qtdMinutosAtual = transformaHora(hora);
     var ehPersistente = horario.split("/");
@@ -74,14 +98,28 @@ function AnalisaHorario (horario){
     }
  }
     //console.log(faltam)
-    return faltam
-    
+    return faltam   
 }
 
-function transformaHora (horario1){
-    var horaMinuto = horario1.split(":");
-    var qtdHoras = parseInt(horaMinuto[0]);
-    var qtdMinutos = parseInt(horaMinuto[1]);
-    qtdMinutos = (qtdHoras*60) + qtdMinutos;
-    return qtdMinutos
+function AnalisaDia (diasAuto){
+    semana = ["DOM","SEG","TER","QUA","QUI","SEX","SAB"]
+    diaHoje = semana.indexOf(diaSemana);
+    console.log("O dia de hoje é: " + diaHoje)
+    var diasFaltam = 0;
+    for(var i =0; i<diasAuto.length; i++){
+        diaAuto = semana.indexOf(diasAuto[i]);
+        if(diaAuto>diaHoje){
+            console.log("O dia da auto é " + diaAuto)
+            diasFaltam = diaAuto - diaHoje;
+            if(diasFaltam==1){
+             var faltam = -1
+            } else {
+            var faltam = parseInt(diasFaltam*60*24)
+            }
+            i = diasAuto.length;
+        }
+    }
+    return faltam;
 }
+
+
