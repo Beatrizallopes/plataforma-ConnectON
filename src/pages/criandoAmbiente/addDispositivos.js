cef1630f56d8cfba2c829a4980cb761be858e599
Beatriz Almeida Lins Lopes <beatriz.lopes@PEINSMTNB2661.insole.local>
2020-07-30 16:30:09 -0300

    Finalizei a página de adicionar dispositivos na criação de ambientes

--- src/pages/criandoAmbiente/addDispositivos.js
+++ src/pages/criandoAmbiente/addDispositivos.js
@@ -12,6 +12,53 @@ var listaDispositivos = [
 const addDispositivos = ({route,navigation}) => {
   // Pegando os dados de navegação
   const {ambiente} = route.params; 
+   // Componente Lista de Dispositivos
+   const ListaDispo = ({listaDispositivos})=> {
+    const item = listaDispositivos.map((dispositivo)=>{
+      const [selecionado,setSelecionado] = useState(false);
+      var jaSel = false;
+      var z = 0; // variável para identificar em que posição de ambiente.dispositivos o dispositivo está
+      // Verificando se o ambiente já está na lista
+      for(var m=0;m<ambiente.dispositivos.length;m++){
+        if(ambiente.dispositivos[m].nome == dispositivo.nome){
+            jaSel = true;
+            z = m;
+        }   
+      }
+      // Se o dispositivo está selecionado ou já está na lista
+      if(selecionado){
+        var urlSelecionado = require('./../../images/icons/ambSel.png');
+        if(!selecionado){
+          var urlSelecionado = require('./../../images/icons/ambNaoSel.png');
+        }
+        if(!jaSel){
+          ambiente.dispositivos.push(dispositivo);
+        }        
+      } else {
+        var urlSelecionado = require('./../../images/icons/ambNaoSel.png');
+        if(jaSel){
+          ambiente.dispositivos.splice(z,1);
+        }
+      }
+    z = z + 1;
+    var marca = dispositivo.marca.toLowerCase();
+         var buscaImagem = require('./../../images/logomarcasDispositivos/sansung.png');
+        return(
+          <View style={styles.dispositivo} key={dispositivo.cod}>
+            <View style={{flexDirection:"row", top:"8%",left:"8%"}}>
+            <Image  source={buscaImagem} style={{marginRight:"20%"}}/>
+            <TouchableWithoutFeedback   onPress={() => {setSelecionado(!selecionado);}}>
+                <Image source={urlSelecionado} style={{top:"-2%"}}></Image>
+            </TouchableWithoutFeedback>
+            </View>
+              <Text style={styles.dispositivoNome}>{dispositivo.nome} </Text>
+            <Text style={styles.dispositivoModelo}>{dispositivo.modelo}</Text>  
+          </View>
+        )
+      })
+      return item;
+          }
+// Agora é a página propriamente dita
        return(
         <ScrollView>
         <Modal animationType="slide" transparent={true} visible={true} >
@@ -19,7 +66,7 @@ const addDispositivos = ({route,navigation}) => {
               <TouchableWithoutFeedback onPress={() => navigation.navigate("Criar Ambiente",{ambiente:ambiente})}>
                 <Image style={styles.fecharModal} source={require("./../../images/icons/fecharModal.png")}></Image>
               </TouchableWithoutFeedback>
-              <View style={{marginVertical:25,flexDirection:"column",left:"5%"}}> 
+              <View style={{marginVertical:25,flexDirection:"column",left:"5%",width:"70%"}}> 
                 <Text style={styles.titulo}> Dispositivos</Text>
                 <Text style={[styles.titulo,{fontSize:22,marginTop:"8%"}]}> Instalados Recentemente</Text>
               </View>
@@ -31,21 +78,7 @@ const addDispositivos = ({route,navigation}) => {
        </ScrollView>
        )
   }
-  // Componente Lista de Dispositivos
-  const ListaDispo = ({listaDispositivos})=> {
-  const item = listaDispositivos.map((dispositivo)=>{
-  var marca = dispositivo.marca.toLowerCase();
-       var buscaImagem = require('../../images/logomarcasDispositivos/sansung.png');
-      return(
-        <View style={styles.dispositivo} key={dispositivo.cod}>
-          <Image  source={buscaImagem} style={{left: "5%",top:"9%"}}/>
-            <Text style={styles.dispositivoNome}>{dispositivo.nome} </Text>
-          <Text style={styles.dispositivoModelo}>{dispositivo.modelo}</Text>  
-        </View>
-      )
-    })
-    return item;
-        }
+ 
 
 const styles = StyleSheet.create({
   body: {
@@ -94,7 +127,7 @@ const styles = StyleSheet.create({
       fontStyle:"normal",
       fontWeight: "600",
       left: "4.76%", 
-      right:"4.76%", 
+      left:"6%", 
       top: "20%",
       lineHeight: 18,
       height: 54,
@@ -102,8 +135,8 @@ const styles = StyleSheet.create({
       letterSpacing: -0.078
     },
     dispositivoModelo: {
-      top: "25%",
-      left: "4.76%",
+      top: "23%",
+      left: "6%",
       color: "rgba(255, 255, 255, 0.5)",
       fontSize: 11,
       lineHeight:13,
